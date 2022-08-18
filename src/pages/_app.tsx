f979ec086113ff9c7435f4430afb1cc3f842560c
Jorge Luis Calleja Alvarado <jorgelca1205@gmail.com>
2022-08-17 19:31:42 -0500

    feat(Docs): Add favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 import { NextPageLayout } from '@/utils/types';
 
@@ -17,7 +18,15 @@ function MyApp({ Component, pageProps }: AppPropsLayout) {
   // propagation and hydration is more beneficial if we mount the layout at this cycle
   const getLayout = Component.getLayout ?? ((page) => page);
 
-  return <StoreProvider>{getLayout(<Component {...pageProps} />)}</StoreProvider>;
+  return (
+    <>
+      <Head>
+        <title>X Boilerplate</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <StoreProvider>{getLayout(<Component {...pageProps} />)}</StoreProvider>
+    </>
+  );
 }
 
 export default MyApp;
---
+++ src/pages/_document.tsx
@@ -0,0 +1,18 @@
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+import * as React from 'react';
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link rel="shortcut icon" href="/favicon.svg" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
