f9a50b9e1da3258f086d11c30d29eef02cfe7a2b
J.L.C.A <jorgelca1205@gmail.com>
2023-03-16 11:30:40 -0600

    refactor(core): Fixed title and use `Metadata` 🎒

---
+++ .changeset/light-dolls-remember.md
@@ -0,0 +1,5 @@
+---
+'x-boilerplate': patch
+---
+
+Fixed title and use `Metadata`
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import type { Metadata } from 'next';
 import * as React from 'react';
 
 import Container from '@/components/Container';
@@ -8,6 +9,53 @@ import StoreProvider from '@/store/StoreProvider';
 
 type MainLayoutProps = {
   children?: React.ReactNode;
+};
+
+export const metadata: Metadata = {
+  title: {
+    default: 'X Boilerplate',
+    template: '%s | X Boilerplate',
+  },
+  description:
+    'X-boilerplate a starting boilerplate with configuration and best practices for your Nextjs projects, so you can only focus on building your product.',
+  openGraph: {
+    title: 'X Boilerplate',
+    description:
+      'X-boilerplate a starting boilerplate with configuration and best practices for your Nextjs projects, so you can only focus on building your product.',
+    url: 'https://beta-x-boilerplate.vercel.app',
+    siteName: 'X Boilerplate',
+    images: [
+      {
+        url: 'https://beta-x-boilerplate.vercel.app/og.jpg',
+        width: 1920,
+        height: 1080,
+      },
+    ],
+    locale: 'en-US',
+    type: 'website',
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+  twitter: {
+    title: 'X Boilerplate',
+    card: 'summary_large_image',
+  },
+  icons: {
+    shortcut: '/favicon.svg',
+  },
+  // verification: {
+  //   google: 'ID',
+  //   yandex: 'yandex',
+  // },
 };
 
 function MainLayout({ children }: MainLayoutProps) {
