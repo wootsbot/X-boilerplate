c7c9ff529e166cbf5da7ee357a8ac5f6abf24f05
Jorge Luis Calleja Alvarado <jorgelca1205@gmail.com>
2024-10-24 15:10:18 -0600

    fix(core): Fixed dialgo use title

---
+++ .changeset/eight-oranges-accept.md
@@ -0,0 +1,5 @@
+---
+'x-boilerplate': patch
+---
+
+Fixed dialgo use title
--- src/components/auth/login-modal.tsx
+++ src/components/auth/login-modal.tsx
@@ -11,6 +11,10 @@ export function LoginModal({ children, onClose }: React.PropsWithChildren<LoginM
       <Dialog.Portal>
         <Dialog.Overlay className="bg-stone-700/80 data-[state=open]:animate-overlayShow fixed inset-0" />
         <Dialog.Content className="data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
+          <Dialog.Title hidden className="text-lg font-semibold">
+            Login
+          </Dialog.Title>
+
           {onClose && (
             <Dialog.Close asChild>
               <button
