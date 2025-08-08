b527baad5f74e0ef6072ee503d206cd336a9cae8
Jorge Luis Calleja Alvarado <jorgelca1205@gmail.com>
2025-08-08 13:05:02 -0600

    chore(core): Bump `sonner`

--- src/components/welcome-toast.tsx
+++ src/components/welcome-toast.tsx
@@ -6,7 +6,6 @@ import { toast } from "sonner";
 
 export function WelcomeToast() {
 	useEffect(() => {
-		// ignore if screen height is too small
 		if (window.innerHeight < 650) return;
 		if (!document.cookie.includes("welcome-toast=2")) {
 			toast("Welcome to X Boilerplate!", {
