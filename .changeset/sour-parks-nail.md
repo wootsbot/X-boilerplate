5998ace6ac544082bc1b10abac676a936ff49255
Jorge Luis Calleja Alvarado <jorgelca1205@gmail.com>
2025-11-12 22:36:11 -0600

    chore(core): Fixed clean config of

---
+++ .changeset/sour-parks-nail.md
@@ -0,0 +1,5 @@
+---
+"x-boilerplate": patch
+---
+
+Fixed clean config of `next-intl`
--- i18n.d.ts
+++
@@ -1,8 +0,0 @@
-import type EN from "./messages/en.json";
-
-type Messages = typeof EN;
-
-declare global {
-  // Use type safe message keys with `next-intl`
-  interface IntlMessages extends Messages {}
-}
